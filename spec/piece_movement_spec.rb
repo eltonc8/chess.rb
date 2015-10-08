bf76b6f401b7b505c681caef72cf784a5b581cfd
eltonc88 <eltonchan@cal.berkeley.edu>
2015-10-08 14:52:44 -0700

    add move tests for Bishop, Pawn

--- spec/piece_movement_spec.rb
+++ spec/piece_movement_spec.rb
@@ -24,7 +24,7 @@ describe "Piece movements" do
       end
 
       it "starting as white has 21 possible moves" do
-        pos = [0, 3]
+        pos = [7, 3]
         piece = Queen.new(:black, pos, board)
         board[pos]= piece
 
@@ -32,15 +32,93 @@ describe "Piece movements" do
       end
 
       it "starting at the center has 25 possible moves" do
-        pos = [5, 3]
+        pos = [5, 5]
         piece = Queen.new(:white, pos, board)
         board[pos]= piece
 
         expect(piece.valid_moves.length).to eq(25)
       end
+
+      it "starting at the center has 25 correct moves" do
+        pos = [5, 5]
+        piece = Queen.new(:white, pos, board)
+        board[pos]= piece
+        expected_moves_I = [[0, 0], [1, 1], [2, 2], [3, 3],
+                            [4, 4], [6, 6], [7, 7]]
+        expected_moves_II = [[3, 7], [4, 6], [6, 4], [7, 3]]
+        expected_vertical_moves = [[0, 5], [1, 5], [2, 5], [3, 5],
+                                   [4, 5], [6, 5], [7, 5]]
+        expected_horizontal_moves = [[5, 0], [5, 1], [5, 2], [5, 3],
+                                     [5, 4], [5, 6], [5, 7]]
+        expected_moves = (
+          expected_moves_I + expected_moves_II +
+          expected_vertical_moves + expected_horizontal_moves
+        ).uniq
+
+        expect(piece.valid_moves.sort).to eq(expected_moves.sort)
+      end
     end
 
     describe "Bishop" do
+      it "starting as black left has 5 diagonal moves of set I" do
+        pos = [0, 2]
+        piece = Bishop.new(:black, pos, board)
+        board[pos]= piece
+        expected_moves = [[1, 3], [2, 4], [3, 5], [4, 6], [5, 7]]
+        valid_moves = piece.valid_moves
+
+        expected_moves.each do |pos|
+          expect(valid_moves).to include(pos)
+        end
+      end
+
+      it "starting as black left has 2 diagonal moves of set II" do
+        pos = [0, 2]
+        piece = Bishop.new(:black, pos, board)
+        board[pos]= piece
+        expected_moves = [[1, 1], [2, 0]]
+        valid_moves = piece.valid_moves
+
+        expected_moves.each do |pos|
+          expect(valid_moves).to include(pos)
+        end
+      end
+
+      it "starting as white right has 5 diagonal moves of set I" do
+        pos = [7, 5]
+        piece = Bishop.new(:white, pos, board)
+        board[pos]= piece
+        expected_moves = [[6, 4], [5, 3], [4, 2], [3, 1], [2, 0]]
+        valid_moves = piece.valid_moves
+
+        expected_moves.each do |pos|
+          expect(valid_moves).to include(pos)
+        end
+      end
+
+      it "starting as white right has 2 diagonal moves of set II" do
+        pos = [7, 5]
+        piece = Bishop.new(:white, pos, board)
+        board[pos]= piece
+        expected_moves = [[6, 6], [5, 7]]
+        valid_moves = piece.valid_moves
+
+        expected_moves.each do |pos|
+          expect(valid_moves).to include(pos)
+        end
+      end
+
+      it "starting at the center has 14 correct moves" do
+        pos = [5, 5]
+        piece = Bishop.new(:white, pos, board)
+        board[pos]= piece
+        expected_moves_I = [[0, 0], [1, 1], [2, 2], [3, 3],
+                            [4, 4], [6, 6], [7, 7]]
+        expected_moves_II = [[3, 7], [4, 6], [6, 4], [7, 3]]
+        expected_moves = expected_moves_I + expected_moves_II
+
+        expect(piece.valid_moves.sort).to eq(expected_moves.sort)
+      end
     end
 
     describe "Rook" do
@@ -108,7 +186,6 @@ describe "Piece movements" do
 
         expect(piece.valid_moves.sort).to eq(expected_moves.sort)
       end
-
     end
   end
 
@@ -196,6 +273,24 @@ describe "Piece movements" do
     end
 
     describe "Pawn" do
+      it "starting as black has 2 possible moves" do
+        pos = [1, 4]
+        piece = Pawn.new(:black, pos, board)
+        board[pos]= piece
+        expected_moves = [[2, 4], [3, 4]]
+
+        expect(piece.valid_moves.sort).to eq(expected_moves.sort)
+      end
+
+      it "starting as white has 2 possible moves" do
+        pos = [6, 4]
+        piece = Pawn.new(:white, pos, board)
+        board[pos]= piece
+        expected_moves = [[4, 4], [5, 4]]
+
+        expect(piece.valid_moves.sort).to eq(expected_moves.sort)
+      end
+
     end
   end
 end
